28872364c33c70dabf5d00240df0939976535e21
Eduardo Revelo <eduardo.revelo@hotmail.com>
2021-09-28 08:39:16 -0700

    Agregar archivos de proyecto.

---
+++ Clases/ApiRest/DBApi.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace Clases.ApiRest
+{
+    public class DBApi
+    {
+        public dynamic Post(string url, string json, string autorizacion = null)
+        {
+            try
+            {
+                var client = new RestClient(url);
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("content-type", "application/json");
+                request.AddParameter("application/json", json, ParameterType.RequestBody);
+                request.Credentials = new NetworkCredential("5f357d4725643258bc5e86e4", "abae22e78428f22a9b094e68e0c29f82");
+                if (autorizacion != null)
+                {
+                    request.AddHeader("Authorization", autorizacion);
+                }
+
+                IRestResponse response = client.Execute(request);
+
+                dynamic datos = JsonConvert.DeserializeObject(response.Content);
+
+                return datos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+
+
+        public dynamic Get(string url)
+        {
+            HttpWebRequest myWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            myWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0";
+            //myWebRequest.CookieContainer = myCookie;
+            myWebRequest.Credentials = new NetworkCredential("5f357d4725643258bc5e86e4", "abae22e78428f22a9b094e68e0c29f82");
+            myWebRequest.Proxy = null;
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)myWebRequest.GetResponse();
+            Stream myStream = myHttpWebResponse.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myStream);
+            //Leemos los datos
+            string Datos = HttpUtility.HtmlDecode(myStreamReader.ReadToEnd());
+
+            dynamic data = JsonConvert.DeserializeObject(Datos);
+
+            return data;
+        }
+    }
+}
---
+++ Clases/Form1.cs
@@ -0,0 +1,63 @@
+﻿using Clases.ApiRest;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clases
+{
+    public partial class Form1 : Form
+    {
+        DBApi dBApi = new DBApi();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrueba_Click(object sender, EventArgs e)
+        {
+            dynamic respuesta = dBApi.Get("https://api.sigfox.com/v2/devices");
+
+            
+          
+            //txtNombreGET.Text = respuesta.data[1].first_name.ToString();
+            txtNombreGET.Text = respuesta.data[0].id.ToString();
+            txtEmail.Text = respuesta.data[0].name.ToString();
+            txtApellidoGET.Text = respuesta.data[0].state.ToString();
+            MessageBox.Show(respuesta.data[0].lqi.ToString());
+        }
+
+        private void btnPost_Click(object sender, EventArgs e)
+        {
+            Persona persona = new Persona
+            {
+                job = txtTrabajador.Text,
+                name = txtNombresPost.Text
+            };
+
+            string json = JsonConvert.SerializeObject(persona);
+
+            dynamic respuesta = dBApi.Post("https://reqres.in/api/users",json);
+
+            MessageBox.Show(respuesta.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Persona
+    {
+        public string name { get; set; }
+        public string job { get; set; }
+    }
+}
+
